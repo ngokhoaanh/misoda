55bd810ef903d7cc5f91295b62817684305e93b6
Daniel DANG <quandangnhu@gmail.com>
2020-06-12 22:02:50 +0200

    add patreon and mail

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { PopoverController } from '@ionic/angular';
 
 import { PopoverPage } from '../about-popover/about-popover';
+import {InAppBrowser} from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'page-about',
@@ -18,7 +19,9 @@ export class AboutPage {
     header: 'Select a Location'
   };
 
-  constructor(public popoverCtrl: PopoverController) { }
+  constructor(public popoverCtrl: PopoverController,
+              public inAppBrowser: InAppBrowser
+  ) { }
 
   async presentPopover(event: Event) {
     const popover = await this.popoverCtrl.create({
@@ -26,5 +29,12 @@ export class AboutPage {
       event
     });
     await popover.present();
+  }
+
+  openExternalUrl(url: string) {
+    this.inAppBrowser.create(
+      url,
+      '_blank'
+    );
   }
 }
